6e260cc4107f035559eecfd37e3c29ea127cdd61
kirill5k <immotional@aol.com>
2019-11-15 19:30:56 +0000

    Initial commit

---
+++ src/main/java/io/kirill/kafkaclient/KafkaConfig.java
@@ -0,0 +1,36 @@
+package io.kirill.kafkaclient;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class KafkaConfig {
+  public static final String MY_TOPIC = "test-topic.v1";
+
+  private static final String SERVER = "127.0.0.1:9092";
+
+  public static Properties defaultProducerProps() {
+    var props = new Properties();
+    props.setProperty(BOOTSTRAP_SERVERS_CONFIG, SERVER);
+    props.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    return props;
+  }
+
+  public static Properties defaultConsumerProps() {
+    var props = new Properties();
+    props.setProperty(BOOTSTRAP_SERVERS_CONFIG, SERVER);
+    props.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    return props;
+  }
+}
---
+++ src/main/java/io/kirill/kafkaclient/KafkaConsumerClient.java
@@ -0,0 +1,11 @@
+package io.kirill.kafkaclient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class KafkaConsumerClient {
+
+  public static void main(String[] args) {
+
+  }
+}
